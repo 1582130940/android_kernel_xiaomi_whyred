1a87fb535a7c0cbf071cada58032045034559fa3
1582130940 <1582130940@qq.com>
2018-10-21 15:39:44 +0800

    drivers: spi: Import Xiaomi Changes

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -89,7 +89,7 @@ struct spidev_data {
 static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
 
-static unsigned bufsiz = 4096;
+static unsigned bufsiz = 4096*8;
 module_param(bufsiz, uint, S_IRUGO);
 MODULE_PARM_DESC(bufsiz, "data bytes in biggest supported SPI message");
 
@@ -123,7 +123,14 @@ spidev_sync_write(struct spidev_data *spidev, size_t len)
 	struct spi_transfer	t = {
 			.tx_buf		= spidev->tx_buffer,
 			.len		= len,
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+			.delay_usecs = 0,
+			.cs_change   = 0,
+			.speed_hz   = 960000,
+#else
 			.speed_hz	= spidev->speed_hz,
+#endif
 		};
 	struct spi_message	m;
 
@@ -163,6 +170,18 @@ spidev_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 	spidev = filp->private_data;
 
 	mutex_lock(&spidev->buf_lock);
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+	if (!spidev->rx_buffer) {
+		spidev->rx_buffer = kmalloc(bufsiz, GFP_KERNEL);
+		if (!spidev->rx_buffer) {
+			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
+			status = -ENOMEM;
+			goto read_unlock;
+		}
+	}
+#endif
+
 	status = spidev_sync_read(spidev, count);
 	if (status > 0) {
 		unsigned long	missing;
@@ -173,6 +192,14 @@ spidev_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 		else
 			status = status - missing;
 	}
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+	kfree(spidev->rx_buffer);
+	spidev->rx_buffer = NULL;
+
+read_unlock:
+#endif
+
 	mutex_unlock(&spidev->buf_lock);
 
 	return status;
@@ -187,18 +214,38 @@ spidev_write(struct file *filp, const char __user *buf,
 	ssize_t			status = 0;
 	unsigned long		missing;
 
-	/* chipselect only toggles at start or end of operation */
-	if (count > bufsiz)
-		return -EMSGSIZE;
+//	/* chipselect only toggles at start or end of operation */
+//	if (count > bufsiz)
+//		return -EMSGSIZE;
 
 	spidev = filp->private_data;
 
 	mutex_lock(&spidev->buf_lock);
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+	if (!spidev->tx_buffer) {
+		spidev->tx_buffer = kmalloc(count, GFP_KERNEL);
+		if (!spidev->tx_buffer) {
+			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
+			status = -ENOMEM;
+			goto write_unlock;
+		}
+	}
+#endif
+
 	missing = copy_from_user(spidev->tx_buffer, buf, count);
 	if (missing == 0)
 		status = spidev_sync_write(spidev, count);
 	else
 		status = -EFAULT;
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+	kfree(spidev->tx_buffer);
+	spidev->tx_buffer = NULL;
+
+write_unlock:
+#endif
+
 	mutex_unlock(&spidev->buf_lock);
 
 	return status;
@@ -224,6 +271,26 @@ static int spidev_message(struct spidev_data *spidev,
 	 * We walk the array of user-provided transfers, using each one
 	 * to initialize a kernel version of the same transfer.
 	 */
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+	if (!spidev->rx_buffer) {
+		spidev->rx_buffer = kmalloc(bufsiz, GFP_KERNEL);
+		if (!spidev->rx_buffer) {
+			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
+			status = -ENOMEM;
+			goto rxbuffer_err;
+		}
+	}
+	if (!spidev->tx_buffer) {
+		spidev->tx_buffer = kmalloc(bufsiz, GFP_KERNEL);
+		if (!spidev->tx_buffer) {
+			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
+			status = -ENOMEM;
+			goto txbuffer_err;
+		}
+	}
+#endif
+
 	tx_buf = spidev->tx_buffer;
 	rx_buf = spidev->rx_buffer;
 	total = 0;
@@ -316,6 +383,17 @@ static int spidev_message(struct spidev_data *spidev,
 	status = total;
 
 done:
+
+#if defined (CONFIG_KERNEL_CUSTOM_WHYRED) || defined (CONFIG_KERNEL_CUSTOM_WAYNE)
+	kfree(spidev->tx_buffer);
+	spidev->tx_buffer = NULL;
+txbuffer_err:
+
+	kfree(spidev->rx_buffer);
+	spidev->rx_buffer = NULL;
+rxbuffer_err:
+#endif
+
 	kfree(k_xfers);
 	return status;
 }
@@ -599,23 +677,23 @@ static int spidev_open(struct inode *inode, struct file *filp)
 		goto err_find_dev;
 	}
 
-	if (!spidev->tx_buffer) {
-		spidev->tx_buffer = kmalloc(bufsiz, GFP_KERNEL);
-		if (!spidev->tx_buffer) {
-			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
-			status = -ENOMEM;
-			goto err_find_dev;
-		}
-	}
+//	if (!spidev->tx_buffer) {
+//		spidev->tx_buffer = kmalloc(bufsiz, GFP_KERNEL);
+//		if (!spidev->tx_buffer) {
+//			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
+//			status = -ENOMEM;
+//			goto err_find_dev;
+//		}
+//	}
 
-	if (!spidev->rx_buffer) {
-		spidev->rx_buffer = kmalloc(bufsiz, GFP_KERNEL);
-		if (!spidev->rx_buffer) {
-			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
-			status = -ENOMEM;
-			goto err_alloc_rx_buf;
-		}
-	}
+//	if (!spidev->rx_buffer) {
+//		spidev->rx_buffer = kmalloc(bufsiz, GFP_KERNEL);
+//		if (!spidev->rx_buffer) {
+//			dev_dbg(&spidev->spi->dev, "open/ENOMEM\n");
+//			status = -ENOMEM;
+//			goto err_alloc_rx_buf;
+//		}
+//	}
 
 	spidev->users++;
 	filp->private_data = spidev;
@@ -624,9 +702,9 @@ static int spidev_open(struct inode *inode, struct file *filp)
 	mutex_unlock(&device_list_lock);
 	return 0;
 
-err_alloc_rx_buf:
-	kfree(spidev->tx_buffer);
-	spidev->tx_buffer = NULL;
+//err_alloc_rx_buf:
+//	kfree(spidev->tx_buffer);
+//	spidev->tx_buffer = NULL;
 err_find_dev:
 	mutex_unlock(&device_list_lock);
 	return status;
@@ -645,11 +723,11 @@ static int spidev_release(struct inode *inode, struct file *filp)
 	if (!spidev->users) {
 		int		dofree;
 
-		kfree(spidev->tx_buffer);
-		spidev->tx_buffer = NULL;
+//		kfree(spidev->tx_buffer);
+//		spidev->tx_buffer = NULL;
 
-		kfree(spidev->rx_buffer);
-		spidev->rx_buffer = NULL;
+//		kfree(spidev->rx_buffer);
+//		spidev->rx_buffer = NULL;
 
 		spin_lock_irq(&spidev->spi_lock);
 		if (spidev->spi)
